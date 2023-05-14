cfc6d28bf2dafded905e662899c7606b6157ea68
Studentjly <3229454821@qq.com>
2023-05-14 15:13:33 +0800

    完成了购物车功能的开发

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import {$http} from '@escook/request-miniprogram'
+import store from '@/store/store.js'
 
 uni.$http = $http
 
@@ -29,6 +30,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-	...App
+	...App,
+  store
 })
 app.$mount()
